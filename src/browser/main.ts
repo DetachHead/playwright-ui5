7701d84e129e7db6a0bf4e698e2eaa10f4c191b6
DetachHead <detachhead@users.noreply.github.com>
2023-07-19 16:54:05 +1000

    support comma-separated rules

--- src/browser/main.ts
+++ src/browser/main.ts
@@ -16,97 +16,94 @@ const parse = createParser({
     },
 })
 
-const queryRule = (root: Element | Document, selector: AstSelector): Element[] => {
-    const rule = throwIfUndefined(selector.rules[0], 'rules array was empty')
-
-    if (rule.tag?.type === 'TagName' && rule.classNames) {
-        // support omitting the sap., since every ui5 control starts with sap (i think):
-        const sapNamespace = 'sap'
-        if (rule.tag.name !== sapNamespace) {
-            rule.tag.name = `${sapNamespace}.${rule.tag.name}`
-        }
-        // hack to prevent parser from treating . as classes:
-        rule.tag.name = [rule.tag.name, ...rule.classNames].join('.')
-        delete rule.classNames
-    }
-
-    const controls = sap.ui.core.Element.registry.filter((element) => {
-        if (rule.ids && rule.ids.length > 1) {
-            throw new Error('multiple ids are not supported')
-        }
-        if (
-            (rule.tag?.type === 'TagName' && rule.tag.name !== element.getMetadata().getName()) ||
-            (rule.ids && rule.ids[0] !== element.getId())
-        ) {
-            return false
-        }
-
-        return (rule.attributes ?? []).every((attr) => {
-            let actualValue: string
-            try {
-                actualValue = String(element.getProperty(attr.name))
-            } catch {
-                // property doesn't exist
-                return false
+const querySelector = (root: Element | Document, selector: AstSelector): Element[] =>
+    selector.rules.flatMap((rule) => {
+        if (rule.tag?.type === 'TagName' && rule.classNames) {
+            // support omitting the sap., since every ui5 control starts with sap (i think):
+            const sapNamespace = 'sap'
+            if (rule.tag.name !== sapNamespace) {
+                rule.tag.name = `${sapNamespace}.${rule.tag.name}`
             }
-            if (!('value' in attr)) {
-                // eg. sap.m.Button[attr]
-                return true
+            // hack to prevent parser from treating . as classes:
+            rule.tag.name = [rule.tag.name, ...rule.classNames].join('.')
+            delete rule.classNames
+        }
+
+        const controls = sap.ui.core.Element.registry.filter((element) => {
+            if (rule.ids && rule.ids.length > 1) {
+                throw new Error('multiple ids are not supported')
             }
-            const expectedValue = (attr.value as AstString).value
-            return {
-                '=': actualValue === expectedValue,
-                '^=': actualValue.startsWith(expectedValue),
-                '$=': actualValue.endsWith(expectedValue),
-                '*=': actualValue.includes(expectedValue),
-                '~=': actualValue.split(/\s+/u).includes(expectedValue),
-                '|=': actualValue.split('-')[0] === expectedValue,
-            }[
-                throwIfUndefined(
-                    attr.operator,
-                    'attribute operator was undefined when value was set (this should NEVER happen)',
-                )
-            ]
-        })
-    })
-    return controls
-        .map((control) => control.getDomRef())
-        .filter((element): element is Element => {
             if (
-                element === null ||
-                // on nested selectors/locators, exclude any elements from outside that scope by making sure they're present in this root:
-                root.querySelector(`#${element.id}`) === null
+                (rule.tag?.type === 'TagName' &&
+                    rule.tag.name !== element.getMetadata().getName()) ||
+                (rule.ids && rule.ids[0] !== element.getId())
             ) {
                 return false
             }
-            if (
-                rule.pseudoClasses &&
-                queryRule(
-                    element,
+
+            return (rule.attributes ?? []).every((attr) => {
+                let actualValue: string
+                try {
+                    actualValue = String(element.getProperty(attr.name))
+                } catch {
+                    // property doesn't exist
+                    return false
+                }
+                if (!('value' in attr)) {
+                    // eg. sap.m.Button[attr]
+                    return true
+                }
+                const expectedValue = (attr.value as AstString).value
+                return {
+                    '=': actualValue === expectedValue,
+                    '^=': actualValue.startsWith(expectedValue),
+                    '$=': actualValue.endsWith(expectedValue),
+                    '*=': actualValue.includes(expectedValue),
+                    '~=': actualValue.split(/\s+/u).includes(expectedValue),
+                    '|=': actualValue.split('-')[0] === expectedValue,
+                }[
                     throwIfUndefined(
-                        rule.pseudoClasses[0],
-                        '":has" pseudo-class was specified without an argument',
-                    ).argument as AstSelector,
-                ).length === 0
-            ) {
-                return false
-            }
-            return true
+                        attr.operator,
+                        'attribute operator was undefined when value was set (this should NEVER happen)',
+                    )
+                ]
+            })
         })
-}
+        return controls
+            .map((control) => control.getDomRef())
+            .filter((element): element is Element => {
+                if (
+                    element === null ||
+                    // on nested selectors/locators, exclude any elements from outside that scope by making sure they're present in this root:
+                    root.querySelector(`#${element.id}`) === null
+                ) {
+                    return false
+                }
+                if (
+                    rule.pseudoClasses &&
+                    querySelector(
+                        element,
+                        throwIfUndefined(
+                            rule.pseudoClasses[0],
+                            '":has" pseudo-class was specified without an argument',
+                        ).argument as AstSelector,
+                    ).length === 0
+                ) {
+                    return false
+                }
+                return true
+            })
+    })
 
 const queryAll = (root: Element | Document, selector: string): Element[] => {
     const parsedSelector = parse(selector)
     if (selector === '') {
         throw new Error('ui5 selector is empty')
     }
-    if (parsedSelector.rules.length > 1) {
-        throw new Error('comma-separated selectors not supported')
-    }
     if (typeof sap === 'undefined') {
         return []
     }
-    return queryRule(root, parsedSelector)
+    return querySelector(root, parsedSelector)
 }
 
 export default {
